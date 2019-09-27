e627e5dfe1624a3707e4c6b82a80a84b9a2aadf1
trobanga <d@niel.re>
2019-09-27 21:19:09 +0200

    Add balance and modify it when new m0ney is added

--- m0ney/src/actions/mod.rs
+++ m0ney/src/actions/mod.rs
@@ -1,13 +1,12 @@
-extern crate clap;
 use clap::{ArgMatches};
 use std::collections::HashMap;
 
 use std::error::Error;
 use std::fs::{File, OpenOptions};
-//extern crate csv;
 
 use chrono;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
+//use serde::de::DeserializeOwned;
 
 enum Actions {
     Show,
@@ -17,14 +16,47 @@ enum Actions {
     Repay,
 }
 
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "PascalCase")]
+#[derive(Debug, Serialize, Deserialize)]
 struct Record<'a> {
-    datetime: &'a str,
+    datetime: String,
     action: &'a str,
-    amount: &'a str,
+    amount: f64,
     description: &'a str,
+    balance: f64
 }
+
+
+impl<'a> Record<'a> {
+    fn new(action: &'a str,
+           amount: f64,
+           description: &'a str,
+           file: &'a str) -> Self {
+        let datetime = chrono::offset::Local::now().to_string();
+        let mut rdr = csv::Reader::from_path(file).unwrap();
+        if let Some(x) = rdr.records().last().unwrap().unwrap().get(4) {
+            let balance = amount + x.parse::<f64>().unwrap();
+            return Record {datetime, action, amount, description, balance};        
+        }
+        panic!("Could not determine new balance.");
+        Record {datetime, action, amount, description, balance: 0.0}
+    }
+}
+
+// impl Record<'_> {
+//     fn new<'a> (datetime: &'a str,
+//                 action: &'a str,
+//                 amount: f64,
+//                 description: &'a str,
+//                 file: &'a str) -> Self {
+//         let mut rdr = csv::Reader::from_path(file).unwrap();
+//         let iter = rdr.deserialize();
+//         let balance = iter.last().unwrap().unwrap();
+//         let d = &String::from(datetime)[..];
+//         let a = &String::from(action);
+//         let dd = &String::from(description);
+//         Record {datetime: d, action: a, amount: amount, description: dd, balance: balance}
+//     }
+// }
 
 pub fn perform_action(matches: &ArgMatches, file: &str) -> Result<(), Box<dyn Error>> {
     match action(&matches) {
@@ -68,17 +100,16 @@ fn show(file: &str) -> Result<(), Box<dyn Error>> {
 fn add<'a>(matches: &ArgMatches<'a>, file: &str) -> Result<(), Box<dyn Error>> {
     if let Some(matches) = matches.subcommand_matches("add") {
         let action = "add";
-        let amount = matches.value_of("amount").unwrap();
+        let amount: f64 = matches.value_of("amount").unwrap().parse::<f64>()?;
         let category = matches.value_of("category").unwrap();
-        let now = chrono::offset::Local::now();
-
-        let record = Record {
-            datetime: &now.to_string(),
-            action: action,
-            amount: amount,
-            description: category
-        };
-
+        
+        let record = Record::new(
+            action,
+            amount,
+            category,
+            file
+        );
+        
         add_record_to_file(&record, file)?;
     }
     
@@ -87,9 +118,12 @@ fn add<'a>(matches: &ArgMatches<'a>, file: &str) -> Result<(), Box<dyn Error>> {
 
 fn add_record_to_file(record: &Record, file: &str) -> Result<(), Box<dyn Error>> {
     let f = OpenOptions::new()
+        .read(true)
         .write(true)
         .append(true)
         .open(file)?;
+
+    
     let mut wtr = csv::WriterBuilder::new()
         .has_headers(false)
         .from_writer(f);
@@ -98,7 +132,6 @@ fn add_record_to_file(record: &Record, file: &str) -> Result<(), Box<dyn Error>>
 
     Ok(())
 }
-
 
 fn borrow(matches: &ArgMatches, file: &str) -> Result<(), Box<dyn Error>> {
 
